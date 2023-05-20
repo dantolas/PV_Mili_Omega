49af700d45ac9589ffb79980469cadbb907ccdee
Charming <kuta.samuel@gmail.com>
2023-05-20 19:00:13 +0200

    Mostly work on GUI

--- app/src/main/java/charming/GUI/BaseFrame.java
+++ app/src/main/java/charming/GUI/BaseFrame.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class BaseFrame extends JFrame{
     public BaseFrame(){
 
-
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         //region <this configs>
         this.setTitle("ChatApp");
         this.setSize(720,480);
@@ -14,8 +14,8 @@ public class BaseFrame extends JFrame{
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         this.setLayout(null); 
-        ImageIcon image = new ImageIcon("/main/resources/images/keboard.icon.png");        
-        this.setIconImage(image.getImage()); 
+        //ImageIcon image = new ImageIcon("app/src/main/resources/images/keboard.icon.png");        
+        //this.setIconImage(image.getImage()); 
         this.getContentPane().setBackground(new Color(0X00004C)); 
         //endregion
 
@@ -24,6 +24,6 @@ public class BaseFrame extends JFrame{
 
     public static void main(String[] args) {
         BaseFrame frame = new BaseFrame();
-        frame.setContentPane(new FirstContentPane());    
+        frame.setContentPane(new FirstContentPane(frame));    
     }
 }
--- app/src/main/java/charming/GUI/FirstContentPane.java
+++ app/src/main/java/charming/GUI/FirstContentPane.java
@@ -1,27 +1,79 @@
 package charming.GUI;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 
 public class FirstContentPane extends JPanel{
 
-    public FirstContentPane(){
+    JFrame parentFrame;
+
+    public FirstContentPane(JFrame parent){
+
+        this.parentFrame = parent;
+
+
+        Color orange = new Color(0xEC5800);
+        Color midBlue = new Color(0x191970);
+
         setLayout(new GridBagLayout());
-        setBackground(new Color(0x191970));
+        setBackground(midBlue);
         setPreferredSize(new Dimension(650,550));
 
+
         JPanel centerPanel = new JPanel();
-        centerPanel.setPreferredSize(new Dimension(300, 300));
-        centerPanel.setBackground(new Color(0xEC5800));
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.setPreferredSize(new Dimension(220, 150));
+        centerPanel.setBackground(orange);
+        
         JButton loginButton = new JButton("Login");
+        loginButton.setFont(new Font("MV Boli",Font.BOLD,30));
+        loginButton.setFocusable(false); 
+        
+        //Border also serves as padding for button text
+        loginButton.setBorder(BorderFactory.createCompoundBorder(new EtchedBorder(), new EmptyBorder(1,5, 1, 5))); 
+        loginButton.setForeground(orange);
+        loginButton.setBackground(midBlue);
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT); 
+        loginButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        loginButton.addActionListener(e -> {
+            //Remove all components, then revalidate to repaint frame
+            parent.getContentPane().removeAll(); 
+            parent.setContentPane(new RegisterContentPane(parent)); 
+            parent.revalidate();
+            parent.repaint();        
+        });
+        
         JButton registerButton = new JButton("Register");
-       
+        registerButton.setFont(new Font("MV Boli",Font.BOLD,30));
+        registerButton.setFocusable(false);
+        
+        //Border also serves as padding for button text
+        registerButton.setBorder(BorderFactory.createCompoundBorder(new EtchedBorder(), new EmptyBorder(1,5, 1, 5))); 
+        registerButton.setForeground(orange);
+        registerButton.setBackground(midBlue);
+        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT); 
+        registerButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        
+        //This is added to function as empty space
+        centerPanel.add(Box.createRigidArea(new Dimension(10,10)));
         centerPanel.add(loginButton);
+        //This is added to function as empty space
+        centerPanel.add(Box.createRigidArea(new Dimension(10,5)));
         centerPanel.add(registerButton);
+        
         add(centerPanel);
         
         
